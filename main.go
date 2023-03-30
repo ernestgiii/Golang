a5e59359fc89050d182c73edfa2e93eb7743b4ac
Ernest Gibbs III <beg0991@yahoo.com>
2023-03-30 08:26:03 -0400

    Golang files

---
+++ main.go
@@ -0,0 +1,34 @@
+// demonstrates Eliza using Golang
+package main
+
+import (
+	"bufio"
+	"firstapp/doctor"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	whatToSay := doctor.Intro()
+
+	fmt.Println(whatToSay)
+
+	for {
+		fmt.Print("-> ")
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+
+	}
+
+}
