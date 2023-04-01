389b0b146106657012f111079a4b3f8ac5779ea4
Ernest Gibbs III <beg0991@yahoo.com>
2023-04-01 08:11:46 -0400

    Web Server script

---
+++ webserver/main.go
@@ -0,0 +1,19 @@
+// This script creates a simple webserver
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	w.Write([]byte("<h1 color=forestgreen>BlackNTech Media</h1>"))
+}
+
+func main() {
+	http.HandleFunc("/hello", Hello)
+
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
